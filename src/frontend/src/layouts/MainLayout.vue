f8bc33dd8f1adfee8e6629bdcfeb9c8acb0d8685
Delpipi <alexandrepaulkouame@gmail.com>
2024-07-18 23:52:53 +0000

    create AuthenticationPage vue

--- src/frontend/src/layouts/MainLayout.vue
+++ src/frontend/src/layouts/MainLayout.vue
@@ -1,106 +1,67 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+  <q-layout>
+    <q-layout-header>
+      <q-toolbar class="text-center" color="primary">
         <q-btn
           flat
           dense
           round
-          icon="menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
           aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
         >
-          Essential Links
-        </q-item-label>
+          <q-icon name="menu" />
+        </q-btn>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
+        <q-toolbar-title class="brand">Flight App</q-toolbar-title>
+        <q-btn
+          flat
+          round
+          dense
+          size="lg"
+          icon="search"
+          :to="{ name: 'home' }"
         />
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
+    >
+      <q-list no-border link inset-delimiter>
+        <q-item :to="{ name: 'home' }" exact>
+          <q-item-side icon="home" />
+          <q-item-main label="Home" sublabel="Home" />
+        </q-item>
+        <q-item :to="{ name: 'profile' }" exact>
+          <q-item-side icon="person" />
+          <q-item-main label="Profile" sublabel="User profile" />
+        </q-item>
+        <q-item :to="{ name: 'bookings' }" exact>
+          <q-item-side icon="flight" />
+          <q-item-main label="My Bookings" sublabel="Bookings" />
+        </q-item>
       </q-list>
-    </q-drawer>
+    </q-layout-drawer>
 
     <q-page-container>
-      <router-view />
+      <transition enter-active-class="animated fadeIn" appear>
+        <router-view />
+      </transition>
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-defineOptions({
-  name: 'MainLayout'
-})
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+<script>
+export default {
+  // name: 'LayoutName',
+  data() {
+    return {
+      leftDrawer: true,
+      leftDrawerOpen: this.$q.platform.is.desktop
+    };
   }
-]
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
+};
 </script>
+
+<style></style>
