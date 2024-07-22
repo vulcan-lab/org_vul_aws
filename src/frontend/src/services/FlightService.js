8551688e1e98c2e568931dbd17d87b34eabcb444
Delpipi <alexandrepaulkouame@gmail.com>
2024-07-22 22:31:31 +0000

    create FlightService.js

---
+++ src/frontend/src/services/FlightService.js
@@ -0,0 +1,42 @@
+import { generateClient } from 'aws-amplify/api';
+import * as mutations from '../graphql/mutations';
+import * as queries from '../graphql/queries';
+
+const client = generateClient();
+
+export const FlightService = {
+    async createFlight(flight) {
+        return await client.graphql({query: mutations.createFlight, variables: {input: flight}});
+    },
+
+    async updateFlight(flight) {
+        return await client.graphql({query: mutations.updateFlight, variables: {input: flight}});
+    },
+
+    async deleteFlight(flightId) {
+       return await client.graphql({query: mutations.deleteFlight, variables: { input: {id: flightId}}});
+    },
+    
+    async getFlight(flightId) {
+        return await client.graphql({query: queries.getFlight, variables: {input : {id : flightId}}});
+    },
+
+    async listFlights(filter) {
+        return await client.graphql({query: queries.listFlights, variables: { filter: filter}});
+    },
+
+    async getFlightBySchedule(departureAirportCode, arrivalAirportCode, departureDate) {
+        return await client.graphql({query: queries.getFlightBySchedule, 
+            variables: { 
+                arrivalAirportCodeDepartureDate: {
+                    beginsWith: {
+                    arrivalAirportCode: arrivalAirportCode, 
+                    departureDate: departureDate
+                    }
+                }, 
+                departureAirportCode: departureAirportCode
+            }
+        })
+    }
+};
+
