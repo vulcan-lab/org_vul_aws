404cffab64cc2a949d376a3fe67d0d6515c7aafa
Delpipi <alexandrepaulkouame@gmail.com>
2024-08-02 10:15:23 +0000

    updated LoyaltService.js

--- src/frontend/src/services/LoyaltyService.js
+++ src/frontend/src/services/LoyaltyService.js
@@ -5,6 +5,11 @@ const client = generateClient();
 
 export const LoyalService = {
     async getLoyalty( userId ) {
-        return await client.graphql({query: getLoyalty, variables: { customer: userId}});
+        try {
+            const res = await client.graphql({query: getLoyalty, variables: { customer: userId}});
+            return res.data.datagetLoyalty.Loyalty;
+        } catch (error) {
+            throw new Error("get customer loyalty failed : ", error.message);
+        }
     }
 }
